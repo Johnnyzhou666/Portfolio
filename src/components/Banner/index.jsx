b051fb5dfd70054cba7bd783cef8a8dc822d9c88
Johnnyzhou666 <140654100+Johnnyzhou666@users.noreply.github.com>
2024-04-13 21:51:27 -0400

    updated

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -4,16 +4,16 @@ export default class Banner extends Component {
     render() {
         return (
             <div className="background1" id="home">
-                <div className="flex-container">
-                    <div className="text-center container">
-                        <br />
-                        <div className="animation1"><h1 className="banner-text">Johnny Zhou</h1></div>
-                        <br />
-                        <br />
-                        <h3>Hi! I'm Johnny, a graduated Software student with a deep passion for technology and innovation. I am keen on discovering and challenging.</h3>
-                    </div>
-                    <img className="banner-img" src={process.env.PUBLIC_URL + '/about.png'} alt="" />
+
+                <div className="text-center container">
+                    <br />
+                    <div className="animation1"><h1 className="banner-text">Johnny Zhou</h1></div>
+                    <br />
+                    <br />
+                    <h3>Hi! I'm Johnny, a graduated Software student with a deep passion for technology and innovation. I am keen on discovering and challenging.</h3>
                 </div>
+                <div style={{ width: "100%" }}><img className="banner-img" src={process.env.PUBLIC_URL + '/about.png'} alt="" /></div>
+
             </div>
         )
     }
