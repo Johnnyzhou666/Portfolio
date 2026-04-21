c07e4b06d7722e8f2389155e28c6ee06ac40219c
Johnny(ZIQUAN) ZHOU <140654100+Johnnyzhou666@users.noreply.github.com>
2026-04-21 03:01:30 -0400

    Update index.jsx

--- src/components/Nav-bar/index.jsx
+++ src/components/Nav-bar/index.jsx
@@ -1,54 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import './index.css';
 
-
 const Navbar = () => {
+
+    const [isBottom, setIsBottom] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+         
+            const bottomReached = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 50;
+            setIsBottom(bottomReached);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+    
+        handleScroll();
+        
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+            <nav className={`navbar navbar-expand-lg navbar-light bg-light ${isBottom ? 'force-contact-active' : ''}`}>
                 <div id="navbarNav" style={{ width: '100%' }}>
                     <ul className="navbar-nav dis">
                         <div className="navbar-header header-top">
                             <a className="navbar-brand" href="https://johnnyzhou666.github.io/Portfolio/" style={{ color: 'rgb(184, 132, 227)' }}>JonyZ</a>
                         </div>
-                        <a href="javascript:void(0)"><li className="nav-item">
-                            <Link className="nav-link" to="home"
-                                spy
-                                smooth={true}
-                                offset={-200}
-                                duration={500} ><i className="fa-solid fa-house"></i>Home</Link>
-                        </li></a>
-                        <a href="javascript:void(0)"><li className="nav-item">
-                            <Link className="nav-link" to="about"
-                                spy
-                                smooth={true}
-                                offset={-100}
-                                duration={500} ><i className="fa-solid fa-user"></i>About</Link>
-                        </li></a>
-                        <a href="javascript:void(0)"><li className="nav-item ">
-                            <Link className="nav-link" to="pro-card"
-                                spy
-                                smooth={true}
-                                offset={-100}
-                                duration={500} ><i className="fa-solid fa-computer"></i>Projects</Link>
-                        </li></a>
-                        <a href="javascript:void(0)"><li className="nav-item ">
-                            <Link className="nav-link" to="skills"
-                                spy
-                                smooth={true}
-                                offset={-100}
-                                duration={500} ><i className="fa-regular fa-thumbs-up"></i>Skills</Link>
-                        </li></a>
-                        <a href="javascript:void(0)"><li className="nav-item ">
-                            <Link className="nav-link" to="letter"
-                                spy
-                                smooth={true}
-                                offset={128}
-                                duration={500} ><i className="fa-regular fa-address-card"></i>Contact</Link>
-                        </li></a>
+                        
+                        <li className="nav-item">
+                            <Link activeClass="active" className="nav-link" to="home" spy={true} smooth={true} offset={-100} duration={500}>
+                                <i className="fa-solid fa-house"></i> Home
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <Link activeClass="active" className="nav-link" to="about" spy={true} smooth={true} offset={-100} duration={500}>
+                                <i className="fa-solid fa-user"></i> About
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <Link activeClass="active" className="nav-link" to="pro-card" spy={true} smooth={true} offset={-100} duration={500}>
+                                <i className="fa-solid fa-computer"></i> Projects
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <Link activeClass="active" className="nav-link" to="skills" spy={true} smooth={true} offset={-100} duration={500}>
+                                <i className="fa-regular fa-thumbs-up"></i> Skills
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item">
+                         
+                            <Link id="contact-nav-link" className="nav-link" to="contact" spy={false} smooth={true} offset={0} duration={500}>
+                                <i className="fa-regular fa-address-card"></i> Contact
+                            </Link>
+                        </li>
+                        
                         <li className="nav-item logo">
-                            <a href="https://github.com/Johnnyzhou666" style={{ textDecoration: 'none' }}><i className="fa-brands fa-github fa-2xl" style={{ color: '#000000' }}></i></a>
+                            <a href="https://github.com/Johnnyzhou666" style={{ textDecoration: 'none' }}>
+                                <i className="fa-brands fa-github fa-2xl" style={{ color: '#000000' }}></i>
+                            </a>
                         </li>
                     </ul>
                 </div>
